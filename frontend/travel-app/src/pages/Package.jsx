1b11f0ea773a8629737a0594a33d074b053c161d
itsmelps <itsmelpsk@gmail.com>
2025-04-04 22:45:04 +0530

    Latest updates

--- frontend/travel-app/src/pages/Package.jsx
+++ frontend/travel-app/src/pages/Package.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
-import Payment from "../components/Payment"
 import Map from "../components/Map"
-import PackageCard from '../components/Description'
-import DatePicker from '../components/DatePicker'
 import Announcement from '../components/Announcement'
+import Description from '../components/Description'
 
 const Package = () => {
   return (
-  <>
-    {/* <PackageCard/> */}
-    {/* <Payment/> */}
-    {/* <Map/> */}
-    {/* <DateRange
-      ranges={selection}
-      onChange={(item) => setSelection([item.selection])}
-      moveRangeOnFirstSelection={false}
-      rangeColors={["#333"]} 
-      showDateDisplay={false}
-    /> */}
-    <Announcement />
-  </>
+    <div className="flex flex-col items-start w-full p-8">
+      <div className="w-full max-w-6xl flex flex-col space-y-8">
+        {/* All components take 2/3 width and are left-aligned */}
+        <div className="w-2/3">
+          <Description />
+        </div>
+        <div className="w-2/3">
+          <Map />
+        </div>
+        <div className="w-2/3">
+          <Announcement />
+        </div>
+      </div>
+    </div>
   )
 }
 
