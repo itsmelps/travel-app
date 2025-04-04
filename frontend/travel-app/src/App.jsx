8fe3c2112e3c0ec5637968a39fce62b43028fc49
itsmelps <itsmelpsk@gmail.com>
2025-04-04 20:39:32 +0530

    Components added

--- frontend/travel-app/src/App.jsx
+++ frontend/travel-app/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Package from "./pages/Package";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/Package" element={<Package />} />
     </Routes>
   );
 };
---
+++ frontend/travel-app/src/pages/Package.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Payment from "../components/Payment"
+import Map from "../components/Map"
+import PackageCard from '../components/Description'
+import DatePicker from '../components/DatePicker'
+import Announcement from '../components/Announcement'
+
+const Package = () => {
+  return (
+  <>
+    {/* <PackageCard/> */}
+    {/* <Payment/> */}
+    {/* <Map/> */}
+    {/* <DateRange
+      ranges={selection}
+      onChange={(item) => setSelection([item.selection])}
+      moveRangeOnFirstSelection={false}
+      rangeColors={["#333"]} 
+      showDateDisplay={false}
+    /> */}
+    <Announcement />
+  </>
+  )
+}
+
+export default Package
